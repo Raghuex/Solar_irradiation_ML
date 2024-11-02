3c2e3ba94bac688cecc2dd5ac0f29351634cccf7
Raghuex <raghuvir.iitg@gmail.com>
2024-11-02 18:49:42 +0530

    Update irradiation_app.py

--- irradiation_app.py
+++ irradiation_app.py
@@ -1,3 +1,22 @@
 import xgboost as xgb
 import streamlit as st 
+import numpy as np
+import pandas as pd
+
 st.title("Solar Irradiation Prediction")
+
+st.info('This is a prediction model based on XGBoost Regressor')
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('cleaned_data.csv')
+  df
+
+  st.write('**X**')
+  X_raw = df.drop('Radiation', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.Radiation
+  y_raw
+
