dec83a6179a91f5351121fdbadb4220a309502fd
Raghuex <raghuvir.iitg@gmail.com>
2024-11-02 19:38:52 +0530

    Update irradiation_app.py

--- irradiation_app.py
+++ irradiation_app.py
@@ -28,3 +28,8 @@ with st.sidebar:
   st.header("Input Features")
   Pressure = st.number_input('Pressure (inHg)', min_value=30.4, max_value=30.6, value=30.5)
   Humidity = st.number_input('Humidity ', min_value=0, max_value=100)
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
