1c41cdf91b4f52e30afb7b5d823df937835ffd28
Raghuex <raghuvir.iitg@gmail.com>
2024-11-03 14:54:52 +0530

    Update irradiation_app.py

--- irradiation_app.py
+++ irradiation_app.py
@@ -110,6 +110,23 @@ transform = {
     
 }
 
+for i in transform:
+  X[i]=transform[i]
+
+#train_test_split
+
+from sklearn.model_selection import train_test_split
+
+# X we are using is the changed one 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+
+  
 
 
 
