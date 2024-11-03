f35452079f346968ec668cdc30d6671eec882b58
Raghuex <raghuvir.iitg@gmail.com>
2024-11-03 13:06:10 +0530

    Update irradiation_app.py

--- irradiation_app.py
+++ irradiation_app.py
@@ -2,6 +2,9 @@ import xgboost as xgb
 import streamlit as st 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 st.title("Solar Irradiation Prediction")
 
@@ -16,12 +19,12 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  X_raw = df.drop(columns=[df.columns[0], 'Radiation'], axis=1)
-  X_raw
+  X = df.drop(columns=[df.columns[0], 'Radiation'], axis=1)
+  X
 
   st.write('**y**')
-  y_raw = df.Radiation
-  y_raw
+  y = df.Radiation
+  y
 
 #Input Features
 with st.sidebar:
@@ -42,6 +45,59 @@ with st.sidebar:
   set_minute = st.number_input('Set Minute', min_value=0, max_value=59, value=0)
 
 
+# Create a dictionary with all input features
+data = {
+    'pressure': pressure,
+    'temperature': temperature,
+    'humidity': humidity,
+    'wind_direction': wind_direction,
+    'speed': speed,
+    'month': month,
+    'day': day,
+    'hour': hour,
+    'minute': minute,
+    'second': second,
+    'rise_hour': rise_hour,
+    'rise_minute': rise_minute,
+    'set_hour': set_hour,
+    'set_minute': set_minute
+}
+
+# Convert the dictionary to a DataFrame
+input_df = pd.DataFrame([data])
+
+features_to_transform = ['Temperature', 'Pressure', 'Humidity', 'Speed', 'WindDirection(Degrees)']
+standard_scaler = StandardScaler()
+min_max_scaler = MinMaxScaler()
+
+for i in features_to_transform:
+    # Create a figure with 5 vertical subplots
+    fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(8, 10))
+    
+    # Original distribution
+    pd.DataFrame(X[i]).hist(ax=ax1, bins=50)
+    ax1.set_ylabel('Normal')
+    
+    # Log transformation
+    pd.DataFrame(np.log(X[i] + 1)).hist(ax=ax2, bins=50)
+    ax2.set_ylabel('Log')
+    
+    # Box-Cox transformation
+    pd.DataFrame(stats.boxcox(X[i] + 1)[0]).hist(ax=ax3, bins=50)
+    ax3.set_ylabel('Box Cox')
+    
+    # StandardScaler transformation
+    scaled_data_standard = standard_scaler.fit_transform(X[[i]])
+    pd.DataFrame(scaled_data_standard).hist(ax=ax4, bins=50)
+    ax4.set_ylabel('Standard')
+    
+    # MinMaxScaler transformation
+    scaled_data_minmax = min_max_scaler.fit_transform(X[[i]])
+    pd.DataFrame(scaled_data_minmax).hist(ax=ax5, bins=50)
+    ax5.set_ylabel('MinMax')
+    
+    # Display the figure in Streamlit
+    st.pyplot(fig)
 
 
 
